27ce7ec7a05c14401d4a8498be4e7ee4a6ca4712
Christopher David <chris@arcade.city>
2022-03-05 22:22:33 -0600

    add rel noreferrer

--- src/components/dom/FullScreenHero.tsx
+++ src/components/dom/FullScreenHero.tsx
@@ -11,6 +11,7 @@ export const FullScreenHero = () => {
             href='https://docs.google.com/document/d/1mVh71Pu82yix8UzUZY1N_pB6gv2byVJhwQERpRHYioI/edit#heading=h.q2b1jbqhh0az'
             className='mt-2 w-64 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-sapphire hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
             target='_blank'
+            rel='noreferrer'
           >
             Read the intro
           </a>
@@ -19,6 +20,7 @@ export const FullScreenHero = () => {
           href='https://discord.com/invite/xTxqZGAj'
           className='mt-6 w-64 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10'
           target='_blank'
+          rel='noreferrer'
         >
           Join the Discord
         </a>
--- src/components/dom/Hero.tsx
+++ src/components/dom/Hero.tsx
@@ -138,6 +138,7 @@ export default function Example() {
                     href='https://docs.google.com/document/d/1mVh71Pu82yix8UzUZY1N_pB6gv2byVJhwQERpRHYioI/edit#heading=h.q2b1jbqhh0az'
                     className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
                     target='_blank'
+                    rel='noreferrer'
                   >
                     Read the intro
                   </a>
